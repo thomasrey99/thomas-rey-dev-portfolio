851b787ce4d050a800ed669532a74d4e4579debc
thomasRey99 <thomas_rey1999@outlook.com>
2023-12-26 20:12:14 -0300

    reset project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import '@/ui/styles/globals.scss'
+import '@/ui/styles/base/globals.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
