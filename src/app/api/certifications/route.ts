76e07f563138698a5aca4d26748dc20752446b97
thomasRey99 <thomas_rey1999@outlook.com>
2023-12-26 23:27:22 -0300

    create path and handler for certificates

--- src/app/api/certifications/route.ts
+++ src/app/api/certifications/route.ts
@@ -1,5 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
+import { getCertifications } from "../controllers/certifications/certificationsController";
 
 export async function GET () {
+
+    const certificates=await getCertifications()
+
+    return NextResponse.json(certificates)
 
 }
---
+++ src/app/api/controllers/certifications/certificationsController.ts
@@ -0,0 +1,25 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/libs/firebase/firebaseConfig";
+
+export const getCertifications=async ()=>{
+
+    try {
+
+        const certificates:Array<Object>=[]
+    
+        const data=await getDocs(collection(db, "certificates"))
+
+        data.forEach((certificate)=>{
+            certificates.push({
+                id:certificate.id,
+                ...certificate.data()
+            })
+        })
+
+        return certificates
+
+    } catch (error) {
+        
+        return {message:"An error occurred when loading the certificates from firebase"}
+    }
+}
