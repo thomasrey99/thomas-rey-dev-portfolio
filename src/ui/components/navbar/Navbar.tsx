fb4d343019fe7714ddf3e85c386edbe0da158d88
thomasRey99 <thomas_rey1999@outlook.com>
2023-12-28 23:20:59 -0300

    complete the About section

--- src/ui/components/navbar/Navbar.tsx
+++ src/ui/components/navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 import style from "@/ui/styles/components/navbar/navbar.module.scss"
 import ProgressBar from "../progressBar/ProgressBar"
 import { NavItems } from './navItems/NavItems';
-
+import Image from "next/image";
+import logo from "@/assets/thomasDevLog.svg"
 
 const Navbar = () => {
 
@@ -9,6 +10,9 @@ const Navbar = () => {
 
   return (
     <nav className={style.navbarCont}>
+        <div className={style.logoCont}>
+          <Image src={logo} alt="thomas rey logo" className={style.logo}/>
+        </div>
         <NavItems navItems={navItems}/>
         <ProgressBar/>
     </nav>
